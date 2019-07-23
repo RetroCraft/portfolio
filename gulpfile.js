afb0163b153d867838fca968d1837645f14aeba5
RetroCraft <retrocraft314@gmail.com>
2019-07-23 15:44:11 -0400

    update or something

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,52 @@
-var gulp          = require('gulp');
-var sass          = require('gulp-sass');
-var sftp          = require('gulp-sftp');
-var changed       = require('gulp-changed');
-var postcss       = require('gulp-postcss');
-var concat        = require('gulp-concat');
-var uglify        = require('gulp-uglify');
-var sourcemaps    = require('gulp-sourcemaps');
-var autoprefixer  = require('autoprefixer');
-var fs            = require('fs')
-var del           = require('del');
-var merge2        = require('merge2');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var changed = require("gulp-changed");
+var postcss = require("gulp-postcss");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require("autoprefixer");
+var del = require("del");
 
-var src = 'src/**';
-var dist = 'dist/';
+var src = "src/**";
+var dist = "docs/";
 
 var uploadable = [src, "!src/css/**", "!src/js/**"];
 
-gulp.task('clean', function () {
-  del.sync(['./dist/**', '!./dist']);
-});
+gulp.task("clean", () => del(["./docs/**", "!./docs"]));
 
-gulp.task('js', function() {
-  gulp.src(['src/js/**/*.js', 'node_modules/skrollr/dist/skrollr.min.js'])
+gulp.task("js", () =>
+  gulp
+    .src(["src/js/**/*.js", "node_modules/skrollr/dist/skrollr.min.js"])
     .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(concat('main.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist/js/'))
-    .pipe(sftp({
-      host: 'retrocraft.ca',
-      user: 'james',
-      key: '/home/james/.ssh/james@retrocraft.ca',
-      remotePath: '/var/www/main/js'
-    }));
-});
+    .pipe(uglify())
+    .pipe(concat("main.js"))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("docs/js/"))
+);
 
-gulp.task('sass', function() {
-  gulp.src('src/css/main.scss')
+gulp.task("sass", () =>
+  gulp
+    .src("src/css/main.scss")
     .pipe(sourcemaps.init())
-      .pipe(sass({ outputStyle: 'compressed' }))
-      .pipe(postcss([ autoprefixer() ]))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist/css/'))
-    .pipe(sftp({
-      host: 'retrocraft.ca',
-      user: 'james',
-      key: '/home/james/.ssh/james@retrocraft.ca',
-      remotePath: '/var/www/main/css'
-    }));
-});
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("docs/css/"))
+);
 
-gulp.task('upload', function () {
-  return gulp.src(uploadable)
+gulp.task("upload", () =>
+  gulp
+    .src(uploadable)
     .pipe(changed(dist))
     .pipe(gulp.dest(dist))
-    .pipe(sftp({
-      host: 'retrocraft.ca',
-      user: 'james',
-      key: '/home/james/.ssh/james@retrocraft.ca',
-      remotePath: '/var/www/main'
-    }));
-});
+);
 
-gulp.task('default', ['clean'], function () {
-  gulp.watch(uploadable, ['upload']);
-  gulp.watch(['src/css/**/*.scss'], ['sass']);
-  gulp.watch(['src/js/*.js'], ['js']);
-})
+gulp.task(
+  "default",
+  gulp.series("clean", () => {
+    gulp.watch(uploadable, gulp.series("upload"));
+    gulp.watch(["src/css/**/*.scss"], gulp.series("sass"));
+    gulp.watch(["src/js/*.js"], gulp.series("js"));
+  })
+);
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var s;
 
@@ -7,60 +7,61 @@ $(document).ready(function() {
   if (usingIE()) {
     $("#IE-modal").modal({
       keyboard: false,
-      backdrop: 'static',
+      backdrop: "static",
       show: true
     });
     $("#IE-vnum").html(usingIE());
     return;
   }
 
-  if (/Chrome/.test(navigator.userAgent))
-    $('#use-chrome').hide();
+  if (/Chrome/.test(navigator.userAgent)) $("#use-chrome").hide();
 
   // Skrollr init
   s = skrollr.init({
     constants: {
-      box: '100p'
+      box: "100p"
     },
-    mobileCheck: function() {return false}
-  });
-
-  // Konami init
-  cheet('↑ ↑ ↓ ↓ ← → ← → b a', function() {
-    $('#konami').css('opacity', 1);
-  });
-
-  cheet('y e s', {
-    next: yesNoProgress,
-    fail: yesNoFail,
-    done: function() {
-      $('#about-yes-no').fadeOut();
-      $('#about-yes').fadeIn();
-      $('#about-ask').fadeOut();
+    mobileCheck: function() {
+      return false;
     }
   });
 
-  cheet('n o', {
+  // Konami init
+  cheet("↑ ↑ ↓ ↓ ← → ← → b a", function() {
+    $("#konami").css("opacity", 1);
+  });
+
+  cheet("y e s", {
     next: yesNoProgress,
     fail: yesNoFail,
     done: function() {
-      $('#about-yes-no').fadeOut();
-      $('#about-no').fadeIn();
-      $('#about-ask').fadeOut();
+      $("#about-yes-no").fadeOut();
+      $("#about-yes").fadeIn();
+      $("#about-ask").fadeOut();
+    }
+  });
+
+  cheet("n o", {
+    next: yesNoProgress,
+    fail: yesNoFail,
+    done: function() {
+      $("#about-yes-no").fadeOut();
+      $("#about-no").fadeIn();
+      $("#about-ask").fadeOut();
     }
   });
 
   function yesNoProgress(str, key, num, seq) {
-    $('#about-yes-no').show();
-    $('#about-no').hide();
-    $('#about-yes').hide();
-    var progress = seq.join('').slice(0, num + 1);
-    $('#about-yes-no').html(progress + " _");
+    $("#about-yes-no").show();
+    $("#about-no").hide();
+    $("#about-yes").hide();
+    var progress = seq.join("").slice(0, num + 1);
+    $("#about-yes-no").html(progress + " _");
   }
 
   function yesNoFail() {
-    $('#about-ask').fadeIn();
-    $('#about-yes-no').html('_');
+    $("#about-ask").fadeIn();
+    $("#about-yes-no").html("_");
   }
 
   // Bootstrap init
@@ -68,62 +69,88 @@ $(document).ready(function() {
 
   // UX init
   randomize();
-  $('.hover-primary')
+  $(".hover-primary")
     .mouseenter(function() {
-      $('.hover').not('.hover-primary').addClass('hover-others');
+      $(".hover")
+        .not(".hover-primary")
+        .addClass("hover-others");
     })
     .mouseleave(function() {
-      $('.hover').not('.hover-primary').removeClass('hover-others');
+      $(".hover")
+        .not(".hover-primary")
+        .removeClass("hover-others");
     });
-  $('.hover-success')
+  $(".hover-success")
     .mouseenter(function() {
-      $('.hover').not('.hover-success').addClass('hover-others');
+      $(".hover")
+        .not(".hover-success")
+        .addClass("hover-others");
     })
     .mouseleave(function() {
-      $('.hover').not('.hover-success').removeClass('hover-others');
+      $(".hover")
+        .not(".hover-success")
+        .removeClass("hover-others");
     });
-  $('.hover-danger')
+  $(".hover-danger")
     .mouseenter(function() {
-      $('.hover').not('.hover-danger').addClass('hover-others');
+      $(".hover")
+        .not(".hover-danger")
+        .addClass("hover-others");
     })
     .mouseleave(function() {
-      $('.hover').not('.hover-danger').removeClass('hover-others');
+      $(".hover")
+        .not(".hover-danger")
+        .removeClass("hover-others");
     });
-  $('.hover-info')
+  $(".hover-info")
     .mouseenter(function() {
-      $('.hover').not('.hover-info').addClass('hover-others');
+      $(".hover")
+        .not(".hover-info")
+        .addClass("hover-others");
     })
     .mouseleave(function() {
-      $('.hover').not('.hover-info').removeClass('hover-others');
+      $(".hover")
+        .not(".hover-info")
+        .removeClass("hover-others");
     });
-  $('.hover-warning')
+  $(".hover-warning")
     .mouseenter(function() {
-      $('.hover').not('.hover-warning').addClass('hover-others');
+      $(".hover")
+        .not(".hover-warning")
+        .addClass("hover-others");
     })
     .mouseleave(function() {
-      $('.hover').not('.hover-warning').removeClass('hover-others');
+      $(".hover")
+        .not(".hover-warning")
+        .removeClass("hover-others");
     });
-  $('.hover-inverse')
+  $(".hover-inverse")
     .mouseenter(function() {
-      $('.hover').not('.hover-inverse').addClass('hover-others');
+      $(".hover")
+        .not(".hover-inverse")
+        .addClass("hover-others");
     })
     .mouseleave(function() {
-      $('.hover').not('.hover-inverse').removeClass('hover-others');
+      $(".hover")
+        .not(".hover-inverse")
+        .removeClass("hover-others");
     });
 });
 
 function toTop() {
   $("body").animate({ scrollTop: 0 }, 1000);
-  $('#konami').css('opacity', 0);
-  $('#about-no, #about-yes').fadeOut();
-  $('#about-ask').fadeIn();
-  $('#about-yes-no').show().html('_');
+  $("#konami").css("opacity", 0);
+  $("#about-no, #about-yes").fadeOut();
+  $("#about-ask").fadeIn();
+  $("#about-yes-no")
+    .show()
+    .html("_");
 
   randomize();
 }
 
 function randomize() {
-  var cards = $('.card-columns .card');
+  var cards = $(".card-columns .card");
   for (var i = 0; i < cards.length; i++) {
     var target1 = Math.floor(Math.random() * cards.length - 1) + 1;
     var target2 = Math.floor(Math.random() * cards.length - 1) + 1;
@@ -132,23 +159,23 @@ function randomize() {
 }
 
 function usingIE() {
-    var ua = window.navigator.userAgent;
+  var ua = window.navigator.userAgent;
 
-    var msie = ua.indexOf('MSIE ');
-    if (msie > 0) {
-        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    }
+  var msie = ua.indexOf("MSIE ");
+  if (msie > 0) {
+    return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+  }
 
-    var trident = ua.indexOf('Trident/');
-    if (trident > 0) {
-        var rv = ua.indexOf('rv:');
-        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    }
+  var trident = ua.indexOf("Trident/");
+  if (trident > 0) {
+    var rv = ua.indexOf("rv:");
+    return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+  }
 
-    var edge = ua.indexOf('Edge/');
-    if (edge > 0) {
-       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-    }
+  var edge = ua.indexOf("Edge/");
+  if (edge > 0) {
+    return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+  }
 
-    return false;
+  return false;
 }
